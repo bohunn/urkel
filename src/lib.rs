f6de2510d783dfd66c0e85b6ea9db484fda9ea48
bohunn <borun.p@hotmail.com>
2020-12-16 20:33:18 +0100

    added connection handling, shows request info

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ mod server;
 mod tests {
     use crate::server;
     use crate::server::{KrServer, KrServerConfig};
+    use std::net::{TcpListener, TcpStream};
+    use std::io::{BufReader, BufWriter, Write, BufRead};
 
     #[test]
     fn it_works() {
@@ -15,12 +17,23 @@ mod tests {
     fn start_server(){
         let config = KrServerConfig{
             server_name: "server1".to_string(),
-            server_address: ("127.0.0.1:7878".to_string()),
+            hostname: ("localhost".to_string()),
+            port: "8080".to_string(),
             instance_count: 1
         };
         let mut server:KrServer = KrServer::new("server1".to_string(), config);
 
         server.listener_start();
+
+        let player_stream = TcpStream::connect("127.0.0.1:8080").expect("Couldn't connect");
+        let mut reader = BufReader::new(&player_stream);
+        let mut response = String::new();
+        reader.read_line(&mut response).expect("Could not read");
+        println!("Player received >{}<", response.trim());
+
+        let mut writer = BufWriter::new(&player_stream);
+        writer.write_all("NAME\n".as_bytes()).expect("Could not write");
+
     }
 }
 
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,44 +1,71 @@
-use std::net::TcpListener;
+use std::net::{TcpStream,TcpListener};
+use std::io::Read;
+use std::thread;
 
 #[derive(Debug)]
 pub(crate) struct KrServerConfig{
     pub(crate) server_name: String,
-    pub(crate) server_address: String,
+    pub(crate) hostname: String,
+    pub(crate) port: String,
     pub(crate) instance_count: i8
 }
 
 struct CurrentRunning(String, KrServerConfig);
+
+impl CurrentRunning {
+    pub(crate) fn init(name: String, server_config: KrServerConfig){
+        CurrentRunning{
+            0: name,
+            1: server_config
+        };
+    }
+}
 
 pub(crate) struct KrServer {
     config: KrServerConfig,
     //add TcpListener which will be started
     state: String
 }
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer=[0;1024];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
 
 impl KrServer{
-
+    //let scontrol = CurrentRunning::init();
     //method to create KrServer and returns it
     pub(crate) fn new(name: String, config: KrServerConfig) -> KrServer {
+        println!("Creating Server:{}, current state is:{}",name,name);
         KrServer{
             config,
             state: "CREATED".to_string(),
         }
     }
 
+
     // method to start listening
     pub(crate) fn listener_start(&mut self) {
         //@TODO:create CurrentRunning entry to send it to server ClusterInfo order
         //cluster info should be a struct containing current state and all running instances with
-        let server_addr = &self.config.server_address;
+        let mut server_addr: String = "".to_string();
+        server_addr.push_str(&self.config.hostname);
+        server_addr.push_str(":");
+        server_addr.push_str(&self.config.port);
+        //= &self.config.hostname + ":" + &self.config.port;
         println!("config parameter: {}", server_addr);
         //let new_cluster_entry = CurrentRunning ("placeholder", config);
         let listener = TcpListener::bind(server_addr).unwrap();
 
         self.state = "LISTENING".to_string();
+        println!("Started listener: {}, current STATE: {}", self.config.server_name, self.state);
         for stream in listener.incoming() {
             let stream = stream.unwrap();
-
             println!("Connection established!");
+            thread::spawn(|| {
+                handle_connection(stream);
+            });
         }
     }
 }
